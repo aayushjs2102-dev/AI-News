aba531b04ebad67b52bc3d2dc4936503a6be97d0
aayushjs2102-dev <aayushjs2102@gmail.com>
2026-07-17 22:22:03 +0530

    Add RSS feed configuration

---
+++ services/rss_services/rss_feeds.py
@@ -0,0 +1,26 @@
+"""
+RSS Feed Configuration
+
+Stores all RSS feed URLs used by the application.
+"""
+
+RSS_FEEDS = {
+
+    "BBC": [
+        "https://feeds.bbci.co.uk/news/rss.xml",
+        "https://feeds.bbci.co.uk/news/technology/rss.xml",
+        "https://feeds.bbci.co.uk/news/world/rss.xml"
+    ],
+
+    "CNN": [
+        "http://rss.cnn.com/rss/edition.rss",
+        "http://rss.cnn.com/rss/edition_technology.rss",
+        "http://rss.cnn.com/rss/edition_world.rss"
+    ],
+
+    "Reuters": [
+        "https://feeds.reuters.com/reuters/topNews",
+        "https://feeds.reuters.com/reuters/technologyNews",
+        "https://feeds.reuters.com/reuters/worldNews"
+    ]
+}
